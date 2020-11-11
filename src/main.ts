ba29e265e0247924c7ea81a9590e000d6a530b9a
Derek <derethehyde@gmail.com>
2020-11-10 19:35:08 -0500

    First pass at a clean looking example of launching an executable and potentially firing off a payload to the Userful API server

--- src/main.ts
+++ src/main.ts
@@ -1,21 +1,33 @@
-import { app, BrowserWindow } from "electron";
+
+import { app, BrowserWindow } from 'electron';
 import * as path from "path";
+
+let mainWindow:BrowserWindow;
 
 function createWindow() {
   // Create the browser window.
-  const mainWindow = new BrowserWindow({
+  mainWindow = new BrowserWindow({
     height: 600,
     webPreferences: {
       preload: path.join(__dirname, "preload.js"),
+      enableRemoteModule: true,
+      nodeIntegration: true,
     },
     width: 800,
+    titleBarStyle: "hidden",
+    frame: false,
+    backgroundColor: '#FFF',
   });
 
   // and load the index.html of the app.
   mainWindow.loadFile(path.join(__dirname, "../index.html"));
 
   // Open the DevTools.
-  mainWindow.webContents.openDevTools();
+  //mainWindow.webContents.openDevTools();
+
+  mainWindow.on('closed', () => {
+    mainWindow = null
+  });
 }
 
 // This method will be called when Electron has finished
@@ -23,14 +35,13 @@ function createWindow() {
 // Some APIs can only be used after this event occurs.
 app.on("ready", () => {
   createWindow();
-
-  app.on("activate", function () {
-    // On macOS it's common to re-create a window in the app when the
-    // dock icon is clicked and there are no other windows open.
-    if (BrowserWindow.getAllWindows().length === 0) createWindow();
-  });
 });
 
+app.on("activate", function () {
+  // On macOS it's common to re-create a window in the app when the
+  // dock icon is clicked and there are no other windows open.
+  if (mainWindow === null) createWindow();
+});
 // Quit when all windows are closed, except on macOS. There, it's common
 // for applications and their menu bar to stay active until the user quits
 // explicitly with Cmd + Q.
@@ -42,3 +53,4 @@ app.on("window-all-closed", () => {
 
 // In this file you can include the rest of your app"s specific main process
 // code. You can also put them in separate files and require them here.
+
--- src/renderer.ts
+++ src/renderer.ts
@@ -1,6 +1,76 @@
+/* eslint-disable @typescript-eslint/no-var-requires */
 // This file is required by the index.html file and will
 // be executed in the renderer process for that window.
 // No Node.js APIs are available in this process unless
 // nodeIntegration is set to true in webPreferences.
 // Use preload.js to selectively enable features
 // needed in the renderer process.
+
+//const win = remote.BrowserWindow()
+const remote = require('electron').remote;
+const win = remote.getCurrentWindow();
+const axios = require('axios');
+const child = require('child_process').execFile;
+
+// When document has loaded, initialise
+document.onreadystatechange = (event:Event) => {
+    if (document.readyState == "complete") {
+        handleWindowControls();
+    }
+};
+
+window.onbeforeunload = (event:Event) => {
+    /* If window is reloaded, remove win event listeners
+    (DOM element listeners get auto garbage collected but not
+    Electron win listeners as the win is not dereferenced unless closed) */
+    win.removeAllListeners();
+}
+
+function handleWindowControls() {
+    document.getElementById('min-button').addEventListener("click", (event:MouseEvent) => {
+        win.minimize();
+    });
+
+    document.getElementById('max-button').addEventListener("click", (event:MouseEvent) => {
+        win.maximize();
+    });
+
+    document.getElementById('restore-button').addEventListener("click", (event:MouseEvent) => {
+        win.unmaximize();
+    });
+
+    document.getElementById('close-button').addEventListener("click", (event:MouseEvent) => {
+        win.close();
+    });
+
+    toggleMaxRestoreButtons();
+    win.on('maximize', toggleMaxRestoreButtons);
+    win.on('unmaximize', toggleMaxRestoreButtons);
+
+    function toggleMaxRestoreButtons() {
+        if (win.isMaximized()) {
+            document.body.classList.add('maximized');
+        } else {
+            document.body.classList.remove('maximized');
+        }
+    }
+}
+
+document.getElementById("send-payload").addEventListener("click", LaunchVNC, false);
+
+function LaunchVNC(){
+  const execPath = "C:\\Windows\\system32\\notepad.exe";
+  SendPayload();
+  child(execPath, async (err:any, data:any) => {
+    if(err){
+      console.error(err);
+      return;
+    } 
+    
+  })
+}
+
+function SendPayload(){
+  //axios.post('http://localhost/')
+  alert('sent payload to userful');
+}
